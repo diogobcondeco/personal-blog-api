ef8d5ef5fbc2e4c25093b81ca51b37649ce9e62d
Diogo Condêço <diogobcondeco@gmail.com>
2024-10-02 12:55:45 +0100

    putting the router in the app.js

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const express = require('express');
+const express = require("express");
+
+// import post router and user router
+const postRouter = require("./routes/post.routes");
+const userRouter = require("./routes/user.routes");
 
 // create express app
 const app = express();
@@ -6,5 +10,9 @@ const app = express();
 // add middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// middleware for API endpoints
+app.use("/api", userRouter);
+app.use("/api/posts", postRouter);
 
 module.exports = app;
