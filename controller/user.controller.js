823ef450b2a4281733d5e5625181a421a4e01117
Diogo Condêço <diogobcondeco@gmail.com>
2024-10-02 12:17:58 +0100

    user controller getAllPosts

---
+++ controller/user.controller.js
@@ -0,0 +1,17 @@
+// import post model
+const Post = require("./../model/Post.model");
+
+exports.getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find({
+      authorId: req.user._id,
+    });
+
+    res.status(200).json({
+      status: "success",
+      posts,
+    });
+  } catch (error) {
+    throw error;
+  }
+};
