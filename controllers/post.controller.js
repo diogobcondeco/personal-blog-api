c29d1f818af3cf3980105d1c56ce1b6db6745a1f
Diogo Condêço <diogobcondeco@gmail.com>
2024-10-02 13:04:04 +0100

    fixed typo

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -17,17 +17,17 @@ exports.getAllPublishedPosts = async (req, res) => {
 }
 
 // get a single post (with state, published)
-exports.getSinglePublishedPost = async (req, res) {
+exports.getSinglePublishedPost = async (req, res) => {
   try {
     const post = await Post.findById(req.params.postId)
-    .where('state')
-    .eq('published');
+      .where("state")
+      .eq("published");
 
     if (!post) {
       return res.status(404).json({
-        status: 'Failed',
-        message: 'Post with given Id not found',
-      })
+        status: "Failed",
+        message: "Post with given Id not found",
+      });
     } else {
       // increment the `readCount` property
       post.readCount === 0 ? post.readCount++ : post.readCount++;
@@ -35,13 +35,13 @@ exports.getSinglePublishedPost = async (req, res) {
     }
 
     res.status(200).json({
-      status: 'success',
+      status: "success",
       post,
     });
   } catch (error) {
     throw error;
   }
-}
+};
 
 // create a post
 exports.createAPost = async (req, res) => {
