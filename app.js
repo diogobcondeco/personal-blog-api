970af428cf06e01df618f57552c90a28d2580da6
Diogo Condêço <diogobcondeco@gmail.com>
2024-10-02 10:20:51 +0100

    base structure with connection to database

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+// create express app
+const app = express();
+
+// add middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+module.exports = app;
